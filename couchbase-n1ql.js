774eafc0b679307bd7e0454ef7de6bcf6f3163cf
Robert McKinnon <robert.mckinnon@astrazeneca.com>
2018-11-02 20:05:22 -0400

    beginnings of adding n1ql support

---
+++ couchbase-n1ql.js
@@ -0,0 +1,51 @@
+var couchbase = require('couchbase');
+const uuidv1 = require('uuid/v1');
+
+module.exports = function(RED) {
+    function couchbaseUpsert(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        var host = config.host;
+        var bucket = config.bucket;
+        var username = this.credentials.username;
+        var password = this.credentials.password;
+
+        var cluster = new couchbase.Cluster('couchbase://' + host + '/');
+        cluster.authenticate(username, password);
+        var activeBucket = cluster.openBucket(bucket, function(err) {
+            if (err) {
+                node.status({fill: 'red', shape: 'ring', text: 'disconnected'});
+            } else {
+                node.status({fill: 'green', shape: 'dot', text: 'connected'});
+            }
+        });
+
+        this.activeBucket = activeBucket;
+        node.on('input', function(msg) {
+            var record_id = null;
+            if (msg.record_id) {
+                record_id = msg.record_id.toString();
+            } else {
+                record_id = uuidv1();
+            }
+            
+            this.activeBucket.upsert(record_id, msg.payload, function(err, result) {
+                if (!err) {
+                    node.send({
+                        payload: result.cas,
+                        original: msg.payload
+                    });
+                } else {
+                   throw "error " + err;
+                }
+            });
+        });
+    }
+    RED.nodes.registerType('upsert', couchbaseUpsert,{
+        credentials: {
+            username: {type: "text"},
+            password: {type: "password"}
+        }
+    });
+}
